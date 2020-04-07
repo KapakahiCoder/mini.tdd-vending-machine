ff664c8b2ad413589063af4a840320bd3a618c2f
Kapakahi <rubbishman@gmail.com>
2020-04-07 22:32:01 +0900

    complete vending machine, mostly

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,128 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+/*
+G & C's to do: list
+  - Have a cash register of available coins for change, seperate from those inserted by the customer
+  - When giving change return the appropriate coinage, not just a rough number
+  - Create a display inventory method for showing the customer what is available
+  - Consider breaking confirmsPurchase method into smaller methods
+  - Clean up purchase flow to allow multiple purchases before returning change
+*/
+
+function VendingMachine() {
+  const juice = { name: `Apple Juice`, price: 350, count: 5 };
+  const coffee = { name: "Tully's", price: 250, count: 7 };
+  const b = { name: "b", price: 250, count: 7 };
+  const c = { name: "c", price: 250, count: 7 };
+  const d = { name: "d", price: 250, count: 7 };
+  const e = { name: "e", price: 250, count: 7 };
+  const f = { name: "f", price: 250, count: 7 };
+  const g = { name: "g", price: 250, count: 7 };
+  const h = { name: "h", price: 250, count: 7 };
+  const i = { name: "i", price: 250, count: 7 };
+  const j = { name: "j", price: 250, count: 7 };
+  const k = { name: "k", price: 250, count: 7 };
+  const l = { name: "l", price: 250, count: 7 };
+  const m = { name: "m", price: 250, count: 7 };
+  const n = { name: "n", price: 250, count: 7 };
+  const o = { name: "o", price: 250, count: 7 };
+
+  const till = {
+    10: 0,
+    50: 0,
+    100: 0,
+    500: 0,
+  };
+  const inventory = [
+    [juice, coffee, b, c],
+    [d, e, f, g],
+    [h, i, j, k],
+    [l, m, n, o],
+  ];
+  let totMoney = 0;
+  let row = "invalid row selection";
+  let column = "invalid column selection";
+
+  return {
+    till: till,
+    insertCoin: function(coin) {
+      if (till[coin] !== undefined) {
+        till[coin]++;
+        totMoney += coin;
+      }
+    },
+    get balance() {
+      return totMoney;
+    },
+    get inventory() {
+      return inventory;
+    },
+    enterRow: function(enteredRow) {
+      if (
+        enteredRow !== "a" &&
+        enteredRow !== "b" &&
+        enteredRow !== "c" &&
+        enteredRow !== "d"
+      ) {
+        row = "invalid row selection";
+        return "invalid row selection";
+      }
+      console.log(enteredRow);
+      row = enteredRow;
+      return row;
+    },
+    enterColumn: function(enteredColumn) {
+      if (
+        enteredColumn !== 1 &&
+        enteredColumn !== 2 &&
+        enteredColumn !== 3 &&
+        enteredColumn !== 4
+      ) {
+        column = "invalid column selection";
+        return "invalid column selection";
+      }
+      console.log(enteredColumn);
+      column = enteredColumn;
+      return column;
+    },
+    get row() {
+      return row;
+    },
+    get column() {
+      return column;
+    },
+    confirmsPurchase: () => {
+      let rowCipher = {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+      };
+      let rowNum = rowCipher[row];
+      if (inventory[rowNum][column - 1].count === 0) {
+        console.log(
+          "Sorry loyal customer we are currently out of " +
+            inventory[rowNum][column - 1].name
+        );
+        console.log("Here is your cash back, " + totMoney + " yen.");
+      } else if (totMoney - inventory[rowNum][column - 1].price < 0) {
+        console.log("Sorry loyal customer we require more funds");
+        console.log("Here is your cash back, " + totMoney + " yen.");
+        console.log("Please start the purchase process again.");
+      } else {
+        totMoney -= inventory[rowNum][column - 1].price;
+        inventory[rowNum][column - 1].count -= 1;
+        console.log("Here is your " + inventory[rowNum][column - 1].name + ".");
+        console.log(
+          "Thank you for your purchase. Your change is " + totMoney + " yen."
+        );
+      }
+      totMoney = 0;
+      row = "invalid row selection";
+      column = "invalid column selection";
+    },
+  };
+}
+
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -18,4 +18,158 @@ describe("vending machine", () => {
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
   });
+  it("Should not accept invalid coins", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.insertCoin(250);
+    //Assert
+    expect(machine.till).to.deep.equal({
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    });
+    expect(machine.balance).to.equal(0); // Use an ES6 getter
+  });
+  it("Should accept coins sequentially", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.insertCoin(500);
+    machine.insertCoin(100);
+    //Assert
+    expect(machine.till).to.deep.equal({
+      10: 0,
+      50: 0,
+      100: 1,
+      500: 1,
+    });
+    expect(machine.balance).to.equal(600); // Use an ES6 getter
+  });
+  it("Should return the inventory array", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    const juice = { name: `Apple Juice`, price: 350, count: 5 };
+    const coffee = { name: "Tully's", price: 250, count: 7 };
+    const b = { name: "b", price: 250, count: 7 };
+    const c = { name: "c", price: 250, count: 7 };
+    const d = { name: "d", price: 250, count: 7 };
+    const e = { name: "e", price: 250, count: 7 };
+    const f = { name: "f", price: 250, count: 7 };
+    const g = { name: "g", price: 250, count: 7 };
+    const h = { name: "h", price: 250, count: 7 };
+    const i = { name: "i", price: 250, count: 7 };
+    const j = { name: "j", price: 250, count: 7 };
+    const k = { name: "k", price: 250, count: 7 };
+    const l = { name: "l", price: 250, count: 7 };
+    const m = { name: "m", price: 250, count: 7 };
+    const n = { name: "n", price: 250, count: 7 };
+    const o = { name: "o", price: 250, count: 7 };
+
+    const inventory = [
+      [juice, coffee, b, c],
+      [d, e, f, g],
+      [h, i, j, k],
+      [l, m, n, o],
+    ];
+
+    expect(machine.inventory).to.eql(inventory);
+  });
+  it("Should accept a row and a column", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.enterRow("a");
+    machine.enterColumn(2);
+    //Assert
+    expect(machine.row).to.equal("a");
+    expect(machine.column).to.equal(2);
+  });
+  it("Should only accept row and column valuees of a-d and 1-4", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.enterRow("e");
+    machine.enterColumn(5);
+    //Assert
+    expect(machine.row).to.equal("invalid row selection");
+    expect(machine.column).to.equal("invalid column selection");
+  });
+  it("Should properly 'dispense' selection(check for valid input, dispense item, remove item from stock give change", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(3);
+    machine.confirmsPurchase();
+    //Assert
+    expect(machine.inventory[0][2].count).equal(6);
+    expect(machine.balance).equal(0);
+    expect(machine.row).equal("invalid row selection");
+    expect(machine.column).equal("invalid column selection");
+  });
+  it("Should return an error message when there is no inventory", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    machine.insertCoin(500);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+    // console.log(machine.inventory[0][0].count)
+
+    //Assert
+    expect(machine.inventory[0][0].count).to.equal(0);
+  });
+  it("Should return an error message when there is insufficient funds", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+    machine.insertCoin(100);
+    machine.enterRow("a");
+    machine.enterColumn(1);
+    machine.confirmsPurchase();
+
+    //Assert
+    expect(machine.inventory[0][0].count).to.equal(5);
+  });
+  it("Should return a balance of 0 at the beginning", () => {
+    //Setup
+    const machine = new VendingMachine();
+
+    //Excercise
+
+    //Assert
+    expect(machine.balance).to.equal(0);
+  });
 });
